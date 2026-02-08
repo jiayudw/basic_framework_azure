bd24c9a954f9427818b23719aa23c916f5903fce
jiayudw <jiayudw0395@gmail.com>
2026-02-08 20:31:28 +0800

    修改部分参数以及cmd逻辑

--- application/robot_def.h
+++ application/robot_def.h
@@ -26,11 +26,11 @@
 
 /* 机器人重要参数定义,注意根据不同机器人进行修改,浮点数需要以.0或f结尾,无符号以u结尾 */
 // 云台参数
-#define YAW_CHASSIS_ALIGN_ECD 6645  // 云台和底盘对齐指向相同方向时的电机编码器值,若对云台有机械改动需要修改 //原始脉冲 (292) - (YAW_CHASSIS_ALIGN_ECD * 360 / 8192) = 0
+#define YAW_CHASSIS_ALIGN_ECD 6632  // 云台和底盘对齐指向相同方向时的电机编码器值,若对云台有机械改动需要修改 //原始脉冲 (292) - (YAW_CHASSIS_ALIGN_ECD * 360 / 8192) = 0 hero为2844
 #define YAW_ECD_GREATER_THAN_4096 0 // ALIGN_ECD值是否大于4096,是为1,否为0;用于计算云台偏转角度
-#define PITCH_HORIZON_ECD 3412      // 云台处于水平位置时编码器值,若对云台有机械改动需要修改
-#define PITCH_MAX_ANGLE 0           // 云台竖直方向最大角度 (注意反馈如果是陀螺仪，则填写陀螺仪的角度)
-#define PITCH_MIN_ANGLE 0           // 云台竖直方向最小角度 (注意反馈如果是陀螺仪，则填写陀螺仪的角度)
+#define PITCH_HORIZON_ECD 3380      // 云台处于水平位置时编码器值,若对云台有机械改动需要修改
+#define PITCH_MAX_ANGLE 35           // 云台竖直方向最大角度 (注意反馈如果是陀螺仪，则填写陀螺仪的角度)
+#define PITCH_MIN_ANGLE -24           // 云台竖直方向最小角度 (注意反馈如果是陀螺仪，则填写陀螺仪的角度)
 // 发射参数
 #define ONE_BULLET_DELTA_ANGLE 45 * 25    // 发射一发弹丸拨盘转动的距离,由机械设计图纸给出
 #define REDUCTION_RATIO_LOADER 49.0f // 拨盘电机的减速比,英雄需要修改为3508的19.0f
